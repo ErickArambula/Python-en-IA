2981910df64a4c412b2252843e593d15d9e0b886
ErickArambula <eric.alejandro@hotmail.com>
2023-09-03 11:29:24 -0600

    Variables 2.0
    Programa final de variables en python

--- 1.Variables.py
+++ 1.Variables.py
@@ -1,3 +1,8 @@
-mensaje = "Bienvenido/a al curso de Python."
-mensaje2 = 10 
-print (mensaje)
+
+mensaje = "Bienvenido/a al curso de Python."  #Se inicializa y adopta un tipo de dato automaticamente
+mensaje = 100
+
+mensaje2 = "Hola mundo, soy el rayo mqueen"
+
+print (mensaje*320)
+# print (mensaje2+200) //Da error por que no puede concatenar los dos tipos de datos 
