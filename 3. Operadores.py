7d1e3ebcd866428c2a0523c382183643153c494d
ErickArambula <eric.alejandro@hotmail.com>
2023-09-03 17:18:04 -0600

    Operadores 2.1

---
+++ 3. Operadores.py
@@ -0,0 +1,15 @@
+#****************************  "+" "-" "*" "/"  ****************************  
+
+suma = 4+8+5+9+5
+resta = 4-6
+multiplicacion = 3*2
+division = 8/7
+
+print(suma + resta + multiplicacion + division,"   <---incorrecto")     #Va a sumaer todos los valores NO los va a concatenar 
+print("\n",suma,"\n",resta,"\n",multiplicacion,"\n",division,"\n")  #Las "," sirven para concatenar datos numericos
+
+#************************Exponentes********************************************
+
+exponente = 2**3          # "**"  Doble asterisco es lo mismo que elevar un numero a ese exponente 
+print(exponente,"\n")
+print(7**2,"\n")          #Manera mas optima de expresarlo 
