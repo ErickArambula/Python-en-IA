2a67dfc0e74400de63a77261e85b5400eed82974
ErickArambula <eric.alejandro@hotmail.com>
2023-09-03 15:24:04 -0600

    Strings 1 _2.0

--- 2. Strings 1.py
+++ 2. Strings 1.py
@@ -1,1 +1,6 @@
-"string develop"
+"Esto es un tring"     #De estas dos maneras se declara un string
+'Esto es un tring'
+
+texto = "Esto es un 'stirng'"  
+texto2 = 'Esto es un "string"'
+print (texto, texto2)
