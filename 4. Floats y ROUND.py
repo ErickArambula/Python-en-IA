43789b91071a64cc654feeeba430dffa11dc21e6
ErickArambula <eric.alejandro@hotmail.com>
2023-09-03 17:25:05 -0600

    FLOATS Y ROUND 2.0

---
+++ 4. Floats y ROUND.py
@@ -0,0 +1,11 @@
+#**********************FLOATS******************
+
+num1 = 10.55
+num2 = 0.84254
+num3 = 64.895
+
+resultado = num1+num2+num3
+
+print(num1+num2)
+print(round(resultado, 1))    #ROUND   round(variable, cant. de decimales)
+
