499ba7bb551b321f74a9bf24031de19ad2058170
ErickArambula <eric.alejandro@hotmail.com>
2023-09-03 15:40:23 -0600

    Cap 3 y 4 Strings

--- 2. Strings 1.py
+++ 2. Strings 1.py
@@ -1,6 +1,18 @@
+#Declaracion de strings
+
 "Esto es un tring"     #De estas dos maneras se declara un string
 'Esto es un tring'
 
 texto = "Esto es un 'stirng'"  
 texto2 = 'Esto es un "string"'
 print (texto, texto2)
+
+#Concatenar Strings
+
+palabra1 = "Rayo"     
+palabra2 = "MQueen"         #Las diferentes maneras de concatenar strings sin necesidad de guardarlo en un avariable 
+
+nombreCanal = palabra1 + "  " + palabra2 + ". "
+
+print(nombreCanal)
+print("Este es el canal" + " de programacion Facil.")
